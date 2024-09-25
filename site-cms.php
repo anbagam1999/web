1a73af576e371a562341117ba6fe60e46b51185d
Imran <team@yieldmore.org>
2024-09-25 14:21:10 +0530

    nonprofit, logo png, slider-data

--- site-cms.php
+++ site-cms.php
@@ -9,7 +9,7 @@ bootstrap([
 	'safeName' => 'anbagam',
 	
 	'support_page_parameters' => true,
-	'version' => [ 'id' => '1', 'date' => '2 Sep 2024', ],
+	'version' => [ 'id' => '2', 'date' => '25 Sep 2024', ],
 
 	'use-parent-slugs' => true,
 	//'home-link-to-section' => true,
@@ -24,24 +24,27 @@ bootstrap([
 	'sections' => ['about-us'],//, 'success-stories', 'galleries', 'shelters'],
 	'no-promotions' => true,
 
-	'theme' => 'biz-land',
+	'theme' => 'cv-nonprofit',
+	'theme-color' => '22B9FE',
 	'image-in-logo' => '-rectangle.jpg',
 
 	'start_year' => '1999',
-	'email' => 'team@anbagam.org',
+	'email' => 'contact@anbagam.org',
 	'phone' => '+91.9444009988',
 	'address' => 'A. Kulandaivel Nachammal Nagar,<br />Thiunillai Village, Post Vichur<br />Chennai 600 103, Tamilnadu',
 	'social' => [
-		[ 'type' => 'group', 'link' => 'https://groups.io/g/yieldmore/topics', 'name' => 'Mailing List from groups.io' ],
-		[ 'type' => 'facebook', 'link' => 'https://www.facebook.com/groups/yieldmore/', 'name' => 'facebook: main group' ],
-		[ 'type' => 'email', 'link' => 'mailto:team@yieldmore.org', 'name' => 'Imran\'s Email' ],
-		[ 'type' => 'phone', 'link' => 'tel:+919841223313', 'name' => 'Imran\'s Mobile (India)' ],
-		[ 'type' => 'youtube', 'link' => 'https://www.youtube.com/@YieldMore', 'name' => 'youtube: main channel love / new' ],
-		[ 'type' => 'linkedin','link' => 'https://www.linkedin.com/groups/14012871/', 'name' => 'YieldMore.org Group on LinkedIn' ],
+		[ 'type' => 'facebook', 'link' => 'https://www.facebook.com/pages/Anbagam-Organization/695733940474317', 'name' => 'facebook: Anbagam Page' ],
+		[ 'type' => 'youtube', 'link' => 'https://www.youtube.com/channel/UCra5W808_ZMHoRfH8M753ug', 'name' => 'youtube: Anbagam Channel' ],
 	],
 
 	'url' => $local ? replace_vars('http://localhost%port%/symphony/anbagam/', 'port') : 'https://anbagam.yieldmore.org/',
 	'path' => SITEPATH,
 ]);
-?>
 
+if (am_var('node') == 'index') {
+	am_var('slider-data', [
+		[ 'h2' => 'Anbagam One', 'link' => '#one', 'linkText' => 'Know One', 'img' => 'slider-one.jpg' ],
+		[ 'h2' => 'Anbagam Two', 'link' => '#two', 'linkText' => 'Know Two', 'img' => 'slider-two.jpg' ],
+	]);
+}
+?>
